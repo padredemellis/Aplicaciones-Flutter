a507b4395e533002e088ab6492b266bc0c898a0e
padredemellis <luisemanuelromeroduarte@gmail.com>
2026-03-24 10:02:46 -0300

    creando nueva pantalla

--- contador/contador/lib/main.dart
+++ contador/contador/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:contador/presentation/screens/counter_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -9,14 +10,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Contador App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen(),
     );
   }
 }
---
+++ contador/contador/lib/presentation/screens/counter_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Esto es un contador')),
+      body: const Center(child: Text('Contador')),
+    );
+  }
+}
